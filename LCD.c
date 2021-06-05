50d32880f714d45510038ad2d8b91d53e7341698
mohamed2919 <85028197+mohamed2919@users.noreply.github.com>
2021-06-05 15:19:41 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -15,6 +15,14 @@ void LCD_VIEWED_DATA(unsigned char data)
 
 // command func.
 
+void LCD_COMMAND(unsigned char command)
+{
+	GPIO_PORTA_DATA_R=0x00;
+	GPIO_PORTB_DATA_R=com;
+	GPIO_PORTA_DATA_R=0x80;
+	delay_micro(0);
+		GPIO_PORTA_DATA_R=0x00;
+	if(command <4) delay_milli(2); else delay_micro(37); }
 
 
 
