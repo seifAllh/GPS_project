f8ccf85a94537d2afafc450514711e1d50737cb8
seifAllh <seifallah.ah@gmail.com>
2021-06-05 23:38:29 +0200

    Update LCD.h

--- LCD.h
+++ LCD.h
@@ -1,3 +1,5 @@
 void LCD_VIEWED_DATA(unsigned char data);
 void LCD_COMMAND(unsigned char command) ;
 void LCD_PORT_Initialization(void);
+void LCD_written_out(void);
+void LCD_numeric_out(int value);
