3d46f5346112695e101042ec71e94655e3cad8cc
seifAllh <seifallah.ah@gmail.com>
2021-06-05 23:55:35 +0200

    Update LED_initi.c

--- LED_initi.c
+++ LED_initi.c
@@ -1,24 +1,28 @@
 # include "tm4c123gh6pm.h"
-#include "macros.h"
+
 
 void LED_INTI (void) {
-	
+
 	SYSCTL_RCGCGPIO_R|=0X20;
 	while((SYSCTL_RCGCGPIO_R&0X20)==0){}
 	  GPIO_PORTF_LOCK_R=0X4c4f434B;
-		SET_BIT(GPIO_PORTF_CR_R,1);
-	  SET_BIT(GPIO_PORTF_DIR_R,1);
-    SET_BIT(GPIO_PORTF_DEN_R,1);
-	  CLEAR_BIT(GPIO_PORTF_AMSEL_R,1);
-	  CLEAR_BIT(GPIO_PORTF_AFSEL_R,1);
-		CLEAR_BIT(GPIO_PORTF_DATA_R,1);
-		GPIO_PORTF_PCTL_R&=~0X000000F0;
-	}
-		
-		// read led 
-	
-	void LED_on (unsigned char x ){
-	SET_BIT(GPIO_PORTF_DATA_R,x);
-	
-	}
+    GPIO_PORTF_CR_R |= 0x0E;
+		GPIO_PORTF_DIR_R |= 0x0E;
+		 GPIO_PORTF_DEN_R |= 0x0E;
+    GPIO_PORTF_AMSEL_R &=~ 0x0E;
+    GPIO_PORTF_AFSEL_R &=~0x0E;
+      GPIO_PORTF_PCTL_R &= ~0x0000FFF0;
+	//	GPIO_PORTF_PUR_R|=0X02;
+
+
+
+}
+
+
+
+		void led_out(unsigned char data ){
+
+
+        	GPIO_PORTF_DATA_R|=data;}
+
 	
