7513e76a97511b2887dcdbd625b164c3b08eb385
aya-mahmod <84995940+aya-mahmod@users.noreply.github.com>
2021-06-05 16:57:42 +0200

    Update LCD.c

--- LCD.c
+++ LCD.c
@@ -35,6 +35,29 @@ void LCD_COMMAND(unsigned char command)
 
 //initialitation
 
+void LCD_PORT_INIT(void){
 
-
-
+	SYSCTL_RCGCGPIO_R|=0x03;
+	// while((SYSCTL_PRGPIO_R&0X03)==0)
+	GPIO_PORTB_DIR_R|=0xFF;
+	GPIO_PORTB_DEN_R|=0xFF;
+	GPIO_PORTB_AMSEL_R&= ~0XFF;
+  GPIO_PORTB_AFSEL_R&= ~0XFF;
+  GPIO_PORTB_PCTL_R&=~0xFFFFFFFF;
+	GPIO_PORTA_DIR_R|=0xE0;
+	GPIO_PORTA_DEN_R|=0xE0;
+  GPIO_PORTA_AMSEL_R&= ~0XE0;
+  GPIO_PORTA_AFSEL_R&= ~0XE0;
+	GPIO_PORTA_PCTL_R&=~0xFFF00000;
+	   delay_micro(37);
+	LCD_COM(0x38);
+	 	 delay_micro(37);
+  LCD_COM(0x06);
+	 	 delay_micro(37);
+  LCD_COM(0x01);
+	 	 delay_micro(37);
+  LCD_COM(0x0F);
+	 	 delay_micro(37);
+	LCD_COM(0x30);
+	 	 delay_micro(37);
+ } 
