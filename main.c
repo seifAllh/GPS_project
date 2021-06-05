4fbbb13f46497a1e77bb7c901448be1cbbc7a528
seifAllh <seifallah.ah@gmail.com>
2021-06-05 23:33:35 +0200

    Update main.c

--- main.c
+++ main.c
@@ -16,35 +16,31 @@ int main (void) {
 
 
 
-	//while(1) {
 
 
 
-	//LCD
 
-	LCD_COMMAND(0x01);
-  LCD_COMMAND(0x80);
-  delay_milli(500);
-	LCD_VIEWED_DATA('d');
-	delay_milli(1);
-	LCD_VIEWED_DATA('0');
-	delay_milli(1);
-	LCD_VIEWED_DATA('n');
-	delay_milli(1);
-	LCD_VIEWED_DATA('e');
-	delay_milli(1);
-	LCD_VIEWED_DATA('=');
-	delay_milli(1);
-LCD_VIEWED_DATA('>');
-	delay_milli(1);
-	LCD_VIEWED_DATA('9');
-	delay_milli(1);
-		LCD_VIEWED_DATA('9');
-	delay_milli(1);
-	LCD_VIEWED_DATA('9');
-	delay_milli(500);
-	// led
-		GPIO_PORTF_DATA_R|=0X06;
+
+
+
+	// distance func.
+
+
+
+
+		//LCD
+		LCD_COMMAND(0x01);
+		LCD_COMMAND(0x80);
+		delay_milli(500);
+
+		LCD_written_out();
+		LCD_numeric_out(127);
+
+		// led
+
+	 GPIO_PORTF_DATA_R|=0X06;
+
+
 
 
 
