cd5eb0b3765ef62d86015f12470ea1ff5174fcff
seifAllh <seifallah.ah@gmail.com>
2021-06-06 12:53:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include "LED.h"
 #include "LCD.h"
 #include "delay.h"
-
+#include "distance.h"
 
 
 void SystemInit(){}
-
+double dis=0;
 int main (void) {
 
 	LED_INTI();
@@ -16,30 +16,21 @@ int main (void) {
 
 
 
+	// distance
 
+  dis= distance_m(40,28,41,29);
 
-
-
-
-
-
-	// distance func.
-
-
-
-
-		//LCD
+	//LCD
+		if( dis>100){
 		LCD_COMMAND(0x01);
-		LCD_COMMAND(0x80);
-		delay_milli(500);
-
+	  LCD_COMMAND(0x80);
+	  delay_milli(500);
 		LCD_written_out();
-		LCD_numeric_out(127);
+		LCD_numeric_out(dis);
 
 		// led
 
-	 GPIO_PORTF_DATA_R|=0X06;
-
+	 GPIO_PORTF_DATA_R|=0X06;}
 
 
 
