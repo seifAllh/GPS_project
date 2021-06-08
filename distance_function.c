8a16681c7b2304d81e05b607aa8d8b8954f06cb6
nourr-aziz <84997629+nourr-aziz@users.noreply.github.com>
2021-06-08 22:08:33 +0200

    Update distance_function.c

--- distance_function.c
+++ distance_function.c
@@ -2,18 +2,18 @@
 #define PI   3.14159265
 #define R    6378.1370
 
-double distance_m(double latitude1, double longitude1, double latitude2,double longitude2)
+long double distance_m(long double latitude1, long double longitude1, long double latitude2,long double longitude2)
 {
     // Converting latitudes and longitudes from degree to radian
-  double latitude1_rad = latitude1*(PI/180);
-    double longitude1_rad = longitude1*(PI/180);
-    double latitude2_rad = latitude2*(PI/180);
-    double longitude2_rad = longitude2*(PI/180);
+  long double latitude1_rad = latitude1*(PI/180);
+    long double longitude1_rad = longitude1*(PI/180);
+    long double latitude2_rad = latitude2*(PI/180);
+    long double longitude2_rad = longitude2*(PI/180);
 
     // Haversine Formula
-  double dlongitude = longitude2_rad - longitude1_rad;
-    double dlatitude = latitude2_rad - latitude1_rad;
-   double ans = pow(sin(dlatitude / 2), 2) +  cos(latitude1) * cos(latitude2) * pow(sin(dlongitude / 2), 2);
+  long double dlongitude = longitude2_rad - longitude1_rad;
+    long double dlatitude = latitude2_rad - latitude1_rad;
+   long double ans = pow(sin(dlatitude / 2), 2) +  cos(latitude1) * cos(latitude2) * pow(sin(dlongitude / 2), 2);
 
               ans = 2 * asin(sqrt(ans));
 
